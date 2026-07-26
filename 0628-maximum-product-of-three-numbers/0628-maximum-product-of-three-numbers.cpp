df891666da3ee21d02a1e9f6bf685ee61e0d2c5c
Shubham Vishwakarma <118911100+Shubham6393@users.noreply.github.com>
2026-07-26 23:48:29 +0530

    Time: 27 ms (5.37%), Space: 31.6 MB (10.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -7,8 +7,8 @@ public:
         int n = nums.size();
 
     return max(
-        nums[n-1] * nums[n-2] * nums[n-3],
-        nums[0] * nums[1] * nums[n-1]
+        nums[n-1] * nums[n-2] * nums[n-3],  // aakhari ka largest element mil jayega
+        nums[0] * nums[1] * nums[n-1]  // agar negative hai toh largest negative mil jayega usko multiply kr denge toh positive mil jayega .. phir usko multiply kr denge last wale se aur dono ka max element nikal lenge ...
     );
         
     }
