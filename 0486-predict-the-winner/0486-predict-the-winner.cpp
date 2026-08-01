89e61283317e549532c3d7d1a0fcaf8c9c385ca6
Shubham Vishwakarma <118911100+Shubham6393@users.noreply.github.com>
2026-08-01 10:44:02 +0530

    Time: 3 ms (39.21%), Space: 9.7 MB (56.39%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool help(int i, int j, int turn, int score, vector<int>& nums) {
+
+        if (i > j) {
+            return score >= 0;
+        }
+
+        if (turn) {
+
+            return help(i + 1, j, 0, score - nums[i], nums) &&
+                   help(i, j - 1, 0, score - nums[j], nums);
+
+        } else {
+
+            return help(i + 1, j, 1, score + nums[i], nums) ||
+                   help(i, j - 1, 1, score + nums[j], nums);
+        }
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        return help(0, n - 1, 0, 0, nums);
+        
+    }
+};
