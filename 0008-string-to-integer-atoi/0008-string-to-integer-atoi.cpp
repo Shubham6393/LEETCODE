86e2e09080ba0b5509d096aeaba4d40441011e32
Shubham Vishwakarma <118911100+Shubham6393@users.noreply.github.com>
2026-08-20 13:28:07 +0530

    Time: 3 ms (13.12%), Space: 9.1 MB (54.92%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,83 +1,70 @@
 class Solution {
 public:
 
-    const int INT_MAX_VAL = 2147483647;
-    const int INT_MIN_VAL = -2147483648;
-    
-    bool isDigit(char c){
+    bool isdigit(char c) {
         return c >= '0' && c <= '9';
     }
+
+    int solve(string &s, int i, int sign, long long num) {
+
+        // Base Case
+        if (i >= s.length() || !isdigit(s[i])) {
+
+            long long result = sign * num;
+
+            // Clamp if overflow
+            if (result <= INT_MIN)
+                return INT_MIN;
+
+            if (result >= INT_MAX)
+                return INT_MAX;
+
+            return (int)result;
+        }
+
+        int digit = s[i] - '0';
+
+        // Check overflow BEFORE num * 10
+        if (num > INT_MAX / 10 ||
+            (num == INT_MAX / 10 && digit > 7)) {
+
+            if (sign == -1)
+                return INT_MIN;
+
+            return INT_MAX;
+        }
+
+        // Add current digit
+        num = num * 10 + digit;
+
+        // Recursive call
+        return solve(s, i + 1, sign, num);
+    }
+
     int myAtoi(string s) {
 
-        // "    123" -> 123
-        // "   -23"  -> -23
-        // "   +123" -> 123
-        // "   -042" -> -42
-        //"   -0b4c2" -> -4
+        int i = 0;
 
-        int i  = 0;
-        int num = 0; // storing the result
-
-        int sign = 1; // default is positive, -1 for negative
-
-        // Case 1 -> Ignore leading Whitespace
-
-        // e.g -> "   -0b4c2"
-        //            i
-
-        while(i<s.length() && (s[i] == ' ')){
+        // Step 1: Ignore leading spaces
+        while (i < s.length() && s[i] == ' ') {
             i++;
         }
-        
-        // Case 2 -> Check for Signed
 
-        // e.g -> "    +123"
-        //             i
+        int sign = 1;
 
-        if(i<s.length()){
-            if(s[i] == '-'){
+        // Step 2: Check sign
+        if (i < s.length()) {
+
+            if (s[i] == '-') {
                 sign = -1;
                 i++;
-            }else if(s[i] == '+'){
+            }
+            else if (s[i] == '+') {
                 i++;
             }
         }
 
-        // Case 3 -> Conversion
-
-        // e.g -> "    -23sc"
-        //              i
-
-        while(i<s.length() && isDigit(s[i])){
-            int digit = s[i] - '0'; // '2' - '0' => 2
-
-            // Case 4 -> Rounding or Overflow condition
-            // -IM ....... 0 .........+IM
-            // 2,147,483,647, agar mai 10 se divide kr du -> 2,147,483,64
-            // -2,147,483,647, agar mai 10 se divide kr du -> -2,147,483,64
-
-            if(num == INT_MAX_VAL / 10){
-                if(sign == 1){
-                    if(digit >= 7){
-                        return INT_MAX_VAL;
-                    }
-                }else if(sign == -1){
-                    if(digit >= 8){
-                        return INT_MIN_VAL;
-                    }
-                }
-            }
-            if(num > INT_MAX_VAL / 10){
-                if(sign == 1){
-                    return INT_MAX_VAL;
-                }else{
-                    return INT_MIN_VAL;
-                }
-            }
-            num = num * 10 + digit; 
-            i++;
-
-        }
-        return sign * num;
+        // Step 3: Start recursion
+        return solve(s, i, sign, 0);
     }
 };
