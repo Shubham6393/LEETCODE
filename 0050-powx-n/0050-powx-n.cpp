42cd9c556f59321ae564dfb194ae2e299ab7fbae
Shubham Vishwakarma <118911100+Shubham6393@users.noreply.github.com>
2026-08-20 15:04:55 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.47%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    double solve(double x, long long n){
+        // base case handle
+        if(n==0){
+            return 1;
+        }
+        // aadha kr diye O(log n)
+        double half = solve(x, n/2);
+        // check kr rhe h even or odd ke liye 
+        if(n % 2 == 0){
+            return half * half;
+        }
+        return x * half * half;
+    }
+    double myPow(double x, int n) {
+        long long N = n;
+        // handle negative case 
+        if(n<0){
+            x = 1/x;
+            N = -N;
+        }
+        return solve(x,n);
+        
+    }
+};
